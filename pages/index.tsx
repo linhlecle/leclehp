077da0396e50b3b89e346682ed904ba37275fa5a
linhtran <linhtran@lecle.co.kr>
2022-03-22 11:12:10 +0700

    support multiple language configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,11 @@
+import { GetStaticPropsContext } from 'next';
+
 export { default } from 'containers/home';
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: (await import(`../messages/home/${locale}.json`)).default,
+    },
+  };
+}
